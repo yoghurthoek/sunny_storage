b57f850e54361ff6f6aa810d30be5fa3858f8708
yoghurthoek <jvdhoek6@gmail.com>
2019-04-05 13:34:19 +0200

    Starting files voor classes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sunny_storage
 Zonnige heuristieken voor een zonnige toekomst!
 waw, super cool!
+
+The grid contains all other classes. To use open thegrid.py.
---
+++ thegrid.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class grid():
+    """attributes"""
+
+    def __init__(self, hood):
+        self.grid = create_grid()
+        self.houses =
+        self.batteries =
+        self.cables =
+
+    def create_grid(self):
+        x = np.linspace(0, 3, 4)
+        y = np.linspace(0, 2, 3)
+        xx, yy = np.meshgrid(x, y)
+
+
+    def load_hood():
