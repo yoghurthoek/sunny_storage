32bee99535f4bc3717534eccfb3bbd3c87615583
yoghurthoek <jvdhoek6@gmail.com>
2019-04-08 17:38:21 +0200

    Voor deel a van het probleem is connect functie toegevoegd die huizen bij batterijen plaats. Niet af, lost wijk 3 niet op

--- house.py
+++ house.py
@@ -1,11 +1,12 @@
 class House(object):
-        """House object housing house things"""
+    """House object housing house things"""
 
-        def __init__(self, id, x, y, output):
-            self.id = id
-            self.posx = x
-            self.posy = y
-            self.output = output
+    def __init__(self, id, x, y, output):
+        self.id = id
+        self.posx = x
+        self.posy = y
+        self.output = output
+        self.pluggedin = False
 
-        def __str__(self):
-            return f"house nr.{self.id}"
+    def __str__(self):
+        return f"house nr.{self.id}"
--- thegrid.py
+++ thegrid.py
@@ -11,7 +11,7 @@ class grid():
         """initializes grid"""
         self.grid = self.create_grid(51, 51)
         self.houses = self.load_houses(
-            f"Huizen&Batterijen/wijk{nr}_huizen.csv")
+            f"Huizen&Batterijen/wijk{nr}_huizensortedhigh-low.csv")
         self.batteries = self.load_batteries(
             f"Huizen&Batterijen/wijk{nr}_batterijen.csv")
 
@@ -24,13 +24,13 @@ class grid():
         """Loads in houses from csv"""
         # create dictionary
         houses = {}
-        with open("Huizen&Batterijen/wijk1_huizen.csv", "r") as f:
+        with open(file, "r") as f:
             reader = csv.reader(f, delimiter=',')
             for row in enumerate(reader, 1):
                 id = row[0]
                 posx = int(row[1][0])
                 posy = int(row[1][1])
-                output = row[1][2]
+                output = float(row[1][2])
 
                 # Make house object with id as key
                 house = House(id, posx, posy, output)
@@ -39,19 +39,18 @@ class grid():
                 # Add house to grid
                 self.grid[posy][posx] = houses[id]
 
-        # voorbeeld voor indexen. Eerst y daarna x. Zou huis nr 0 moeten geven.
-        print(self.grid[47][34])
+        return houses
 
     def load_batteries(self, file):
         """Loads in batteries from csv"""
         batteries = {}
-        with open("Huizen&Batterijen/wijk2_batterijen.csv", "r") as f:
+        with open(file, "r") as f:
             reader = csv.reader(f, delimiter=',')
             for row in enumerate(reader, 1):
                 id = row[0]
                 posx = int(row[1][0])
                 posy = int(row[1][1])
-                capacity = row[1][2]
+                capacity = float(row[1][2])
 
                 # Make battery object with id as key
                 battery = Battery(id, posx, posy, capacity)
@@ -60,12 +59,34 @@ class grid():
                 # Add battery to the grid
                 self.grid[posy][posx] = batteries[id]
 
-        print(batteries[1])
+        return batteries
+
+    def connect(self):
+        for key in self.batteries:
+            count = 0
+            backcount = 150
+            while self.batteries[key].filled < self.batteries[key].capacity:
+                count += 1
+                if self.batteries[key].capacity - self.batteries[key].filled < self.houses[150].output:
+                    break
+                if self.houses[count].output + self.batteries[key].filled < self.batteries[key].capacity and self.houses[count].pluggedin is False:
+                    self.batteries[key].connected.append(self.houses[count])
+                    self.batteries[key].filled += self.houses[count].output
+                    self.houses[count].pluggedin = self.batteries[key]
+
+        for key in self.batteries:
+            print(self.batteries[key].filled)
+            for house in self.batteries[key].connected:
+                print(house)
+
+        for house in self.houses:
+            print(self.houses[house].pluggedin)
 
 
 if __name__ == "__main__":
     if len(argv) == 2:
         if argv[1] == '1' or argv[1] == '2' or argv[1] == '3':
             grid = grid(argv[1])
+            grid.connect()
     else:
         print("not correct input")
