d4589bfb355e4b9d90814c8219852578f1a84444
Feline Benavides <feline.benavides@student.uva.nl>
2019-04-29 10:25:41 +0200

    push

--- Algorithms/astar.py
+++ Algorithms/astar.py
@@ -14,6 +14,8 @@ Werkt nog van geen kant :D, want problems:
     # of eerst koppelen en daarna snelste pad berekenen
         # maar dan wil je optimale sort functie
 
+open list: huis connecten met batt (kies beste optie)?
+
 """
 
 # start en goal maar even gedefinieerd, dist is afstand tussen alle
--- Algorithms/dfs.py
+++ Algorithms/dfs.py
@@ -4,3 +4,6 @@ Simpelere dfs
 
 def dfs(b, h, dist):
     print("hello world")
+
+    # even die pagina opzoeken waar bfs + 1 if statement stond
+    
