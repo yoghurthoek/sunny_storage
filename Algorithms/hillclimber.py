495aa77ce066f930116bed052761c5093f89dc39
yoghurthoek <jvdhoek6@gmail.com>
2019-05-03 15:02:20 +0200

    Fixed price calculation hillclimber

--- Algorithms/hillclimber.py
+++ Algorithms/hillclimber.py
@@ -1,21 +1,23 @@
 def hillclimber(dist, distdict, price, b, h):
 
     for nr1 in h:
-            for nr2 in h:
-                # Checkt of huizen niet al bij zelfde batterij zitten
-                if not h[nr1].pluggedin == h[nr2].pluggedin:
-                    if (b[h[nr1].pluggedin.id].filled - h[nr1].output + h[nr2].output < b[h[nr1].pluggedin.id].capacity) and (b[h[nr2].pluggedin.id].filled - h[nr2].output + h[nr1].output < b[h[nr2].pluggedin.id].capacity):
-                        if distdict[nr1][h[nr1].pluggedin.id] + distdict[nr2][h[nr2].pluggedin.id] > distdict[nr1][h[nr2].pluggedin.id] + distdict[nr2][h[nr1].pluggedin.id]:
+        for nr2 in h:
+            # Checkt of huizen niet al bij zelfde batterij zitten
+            if not h[nr1].pluggedin == h[nr2].pluggedin:
+                if (b[h[nr1].pluggedin.id].filled - h[nr1].output + h[nr2].output < b[h[nr1].pluggedin.id].capacity) and (b[h[nr2].pluggedin.id].filled - h[nr2].output + h[nr1].output < b[h[nr2].pluggedin.id].capacity):
+                    if distdict[nr1][h[nr1].pluggedin.id] + distdict[nr2][h[nr2].pluggedin.id] > distdict[nr1][h[nr2].pluggedin.id] + distdict[nr2][h[nr1].pluggedin.id]:
 
-                            b[h[nr2].pluggedin.id].connected.remove(h[nr2])
-                            b[h[nr2].pluggedin.id].connected.append(h[nr1])
+                        b[h[nr2].pluggedin.id].connected.remove(h[nr2])
+                        b[h[nr2].pluggedin.id].connected.append(h[nr1])
 
-                            b[h[nr1].pluggedin.id].connected.remove(h[nr1])
-                            b[h[nr1].pluggedin.id].connected.append(h[nr2])
+                        b[h[nr1].pluggedin.id].connected.remove(h[nr1])
+                        b[h[nr1].pluggedin.id].connected.append(h[nr2])
 
-                            price = price - (dist[nr1][h[nr1].pluggedin.id][0] + dist[nr2][h[nr2].pluggedin.id][0]) * 9 + (dist[nr1][h[nr2].pluggedin.id][0] + dist[nr2][h[nr1].pluggedin.id][0]) * 9
-
-                            temporary = h[nr2].pluggedin
-                            h[nr2].pluggedin = h[nr1].pluggedin
-                            h[nr1].pluggedin = temporary
+                        temporary = h[nr2].pluggedin
+                        h[nr2].pluggedin = h[nr1].pluggedin
+                        h[nr1].pluggedin = temporary
+    price = 0
+    for bat in b:
+        for house in b[bat].connected:
+            price += distdict[house.id][house.pluggedin.id] * 9
     return price
