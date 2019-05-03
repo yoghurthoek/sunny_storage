f7b405a43e94398f055ec323baa14409c0ec2dc5
SnowleopardB <b.j.g.akerboom@gmail.com>
2019-05-03 13:50:39 +0200

    greedy

--- Algorithms/greedy.py
+++ Algorithms/greedy.py
@@ -1,17 +1,16 @@
+import random
+
 def Greedy(dist, b, h):
     price = 0
+    keylist = list(range(0, 150))
     for nr in h:
-        for cell in dist[nr]:
-            if b[cell[1]].filled + h[nr].output < b[cell[1]].capacity:
-                b[cell[1]].connected.append(h[nr])
+        key = random.choice(keylist)
+        keylist.remove(key)
+        for cell in dist[key]:
+            if b[cell[1]].filled + h[key].output < b[cell[1]].capacity:
+                b[cell[1]].connected.append(h[key])
                 b[cell[1]].filled += h[nr].output
-                h[nr].pluggedin = b[cell[1]]
+                h[key].pluggedin = b[cell[1]]
                 price += cell[0] * 9
                 break
     return price, b, h
-
-    # Checks
-    # for key in b:
-    #     print(b[key].filled)
-    # for house in h:
-    #     print(h[house].pluggedin)
