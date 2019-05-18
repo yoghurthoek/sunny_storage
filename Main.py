715f45544db65d8b61c4fc4b0e37e76e36459a3e
yoghurthoek <jvdhoek6@gmail.com>
2019-05-18 10:35:33 +0200

    last changes to main

--- Main.py
+++ Main.py
@@ -159,6 +159,15 @@ def input_kmeans(batteries, houses):
     k = int(input("> "))
     KmeansClusterdistance(houses, batteries, k)
 
+def input_batoptimize(batteries, houses, command, base):
+    command = base + "-->" + "HILLCLIMBER" + "-->" + "OPTIMIZE"
+    input_hillclimber(batteries, houses, "HILLCLIMBER", base)
+    battery_optimization(batteries)
+    dist, distdict, lowbprice = distancearr(batteries, houses)
+    price = price_calc(batteries, distdict)
+    write_to_csv(argv[1], command, price)
+    print(price)
+    visualize(batteries, houses)
 
 if __name__ == "__main__":
     if len(argv) == 2:
@@ -175,7 +184,9 @@ choices:
     depth-first
     hillclimber
     randclimber
-    multiplehillclimber""")
+    multiplehillclimber
+    Kmeansclusterdistance
+    optimize""")
     functions = {
         "RANDOM": input_random,
         "FIRST-FIT": input_firstfit,
@@ -186,7 +197,8 @@ choices:
         "HILLCLIMBER": input_hillclimber,
         "RANDCLIMBER": input_randclimber,
         "MULTIPLEHILLCLIMBER": input_multclimber,
-        "KMEANSCLUSTERDISTANCE": input_kmeans
+        "KMEANSCLUSTERDISTANCE": input_kmeans,
+        "OPTIMIZE": input_batoptimize
     }
     command = (input("> ")).upper()
     try:
@@ -195,115 +207,5 @@ choices:
         print("base: greedy or random")
         base = (input("> ")).upper()
         functions[command](grid.batteries, grid.houses, command, base)
-
-    #         if command == "RANDOM":
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             random_alg(distdict, grid.batteries, grid.houses)
-    #             price = price_calc(grid.batteries, distdict)
-    #             print(price)
-    #             visualize(grid.batteries, grid.houses)
-    #         elif command == "FIRST_FIT":
-    #             Decreasingfirstfit(grid, grid.batteries, grid.houses)
-    #             visualize(grid.batteries, grid.houses)
-    #         elif command == "AVERAGE-FIT":
-    #             Averagefit(grid, grid.batteries, grid.houses)
-    #             visualize(grid.batteries, grid.houses)
-    #         elif command == "GREEDY":
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             Greedy(dist, grid.batteries, grid.houses)
-    #             price = price_calc(grid.batteries, distdict)
-    #             print(price)
-    #             visualize(grid.batteries, grid.houses)
-    #             # grid.savefig('hallo.png') # plus 1 voor elke run?
-    #         elif command == "BREADTH-FIRST":
-    #             node = Node()
-    #             best = Node()
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             best.price = Greedy(dist, grid.batteries, grid.houses)
-    #             bfs(node, grid.batteries, grid.houses, distdict, best)
-    #             visualize(grid.batteries, grid.houses)
-    #         elif command == "DEPTH-FIRST":
-    #             node = Node()
-    #             best = Node()
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             best.price = 700000
-    #             node.lowbound = lowbprice
-    #             dfs(node, grid.batteries, grid.houses, distdict, dist, best)
-    #             price = price_calc(grid.batteries, distdict)
-    #             visualize(grid.batteries, grid.houses)
-    #         elif command == "HILLCLIMBER":
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             print("""what is the base?
-    #                         choices:
-    #                         random
-    #                         greedy
-    #                         """)
-    #             command2 = (input("> ")).upper()
-    #             if command2 == "RANDOM":
-    #                 random_alg(distdict, grid.batteries, grid.houses)
-    #                 hillclimber(dist, distdict, grid.batteries, grid.houses)
-    #                 price = price_calc(grid.batteries, distdict)
-    #                 print(price)
-    #                 visualize(grid.batteries, grid.houses)
-    #             elif command2 == "GREEDY":
-    #                 Greedy(dist, grid.batteries, grid.houses)
-    #                 hillclimber(dist, distdict, grid.batteries, grid.houses)
-    #                 price = price_calc(grid.batteries, distdict)
-    #                 print(price)
-    #                 visualize(grid.batteries, grid.houses)
-    #             else:
-    #                 print("invalid command")
-    #             print("""Do you want to optimize the location of the batteries with hillclimber?
-    #         Optimize
-    #         no optimize
-    #         """)
-    #             command3 = (input("> ")).upper()
-    #             if command3 == "OPTIMIZE":
-    #                 dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #                 battery_optimization(grid.batteries)
-    #                 dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #                 price = price_calc(grid.batteries, distdict)
-    #                 print(price)
-    #                 visualize(grid.batteries, grid.houses)
-    #             elif command == "NO OPTIMIZE":
-    #                 print("nothing")
-    #         elif command == "RANDCLIMBER":
-    #             print("repeat until no change for how many times?")
-    #             repetitions = int(input("> "))
-    #             dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #             Greedy(dist, grid.batteries, grid.houses)
-    #             price = price_calc(grid.batteries, distdict)
-    #             print(f"Price of greedy: {price}")
-    #             Randclimber(repetitions, distdict, grid.batteries, grid.houses)
-    #             price = price_calc(grid.batteries, distdict)
-    #             print(f"Price after climbing: {price}")
-    #             visualize(grid.batteries, grid.houses)
-    #         # elif command == "MULTIPLEHILLCLIMBER":
-    #         #     print("repeat until no change for how many times?")
-    #         #     repetitions = int(input("> "))
-    #         #     dist, distdict, lowbprice = distancearr(grid.batteries, grid.houses)
-    #         #     Greedy(dist, grid.batteries, grid.houses)
-    #         #     price = price_calc(grid.batteries, distdict)
-    #         #     node = Node()
-    #         #     node.fillnode(grid.batteries, grid.houses, price)
-    #         #     print(f"Price of greedy: {price}")
-    #         #     multhillclimber(node, repetitions, distdict, grid.batteries, grid.houses)
-    #         #     price = price_calc(grid.batteries, distdict)
-    #         #     print(f"Price after climbing: {price}")
-    #         #     visualize(grid.batteries, grid.houses)
-    #         else:
-    #             print("invalid command")
-    #
-    #         print("""do you want to save your run?
-    #         Options:
-    #         Yes
-    #         No
-    #         """)
-    #         if (input("> ")).upper() == "YES":
-    #             algorithm = (command + " --> " + command2 + " --> " + command3)
-    #             write_to_csv(argv[1], algorithm, price)
-    #             # save fig
-    #     else:
-    #         print("neigborhood doesn't exist")
-    # else:
-    #     print("not correct input")
+    except KeyError:
+        print("invalid command")
