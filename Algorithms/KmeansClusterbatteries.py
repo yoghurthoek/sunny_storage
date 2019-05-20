4eb16bbfb96b194ca4328029f6df348a32739589
SnowleopardB <b.j.g.akerboom@gmail.com>
2019-05-20 12:11:42 +0200

    waw

--- Algorithms/KmeansClusterbatteries.py
+++ Algorithms/KmeansClusterbatteries.py
@@ -1,22 +1,49 @@
-import pandas as pd
-import numpy
+def KmeansClusterbatteries(h, b, k)
 
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+    clustercenters = []
+    keylist = list(range(0, 50))
+    housecoordinates = {}
+    k_list = list(range(0, k))
+    for number in k_list:
+        clustercenters.append([0,0,0])
+        clustercenters[number][0] = random.choice(keylist)
+        clustercenters[number][1] = random.choice(keylist)
+        clustercenters[number][2] = number
+        housecoordinates[number] = []
+
+    startover = 0
+    while startover < k:
+        for number in k_list:
+            housecoordinates[number] = []
+        for house in h:
+            manhatbest = 1000
+            for p in clustercenters:
+                manhat = abs(p[0] - h[house].posx) + \
+                         abs(p[1] - h[house].posy)
+                if manhat < manhatbest:
+                    manhatbest = manhat
+                    bestp = p[2]
+            housecoordinates[bestp].append(h[house].id)
+        print(clustercenters, housecoordinates) 
 
 
-Data = {'x': [],
-        'y': []
-       }
-for i in grid.houses:
-    Data('x').append(thegrid.houses[i].posx)
-    Data('Y').append(thegrid.houses[i].posy)
+        startover = 0
+        oldclustercenters = deepcopy(clustercenters)
+        for i in k_list:
+            totaly = 0
+            totalx = 0
+            totalh = 0
+            if len(housecoordinates[i]) != 0:
+                for houseID in housecoordinates[i]:
+                    house = h[houseID]
+                    totalx += house.posx
+                    totaly += house.posy
+                    totalh += 1
+                clustercenters[i][0] = round(totalx/totalh)
+                clustercenters[i][1] = round(totaly/totalh)
 
-df = pd(Data,columns=['x','y'])
+            if oldclustercenters[i][0] == clustercenters[i][0] and oldclustercenters[i][1] == clustercenters[i][1]:
+                startover += 1
 
-kmeans = KMeans(n_clusters=4).fit(df)
-centroids = kmeans.cluster_centers_
-print(centroids)
 
-plt.scatter(df['x'], df['y'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
-plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
+    return clustercenters, housecoordinates
