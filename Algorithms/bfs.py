cfeb1e8eccc635f0dddcc06e97d2d3957d2868c4
Feline Benavides <feline.benavides@student.uva.nl>
2019-05-23 19:05:08 +0200

    Update to do list

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -11,22 +11,17 @@ def bfs(startnode, b, h, distdict, best):
     start = time.time()
     max = len(h)
     nrofnodes = 0
-    # Create a queue / or stack
     queue = deque([startnode])
-    # while queue is not empty
     while queue:
         try:
-            # Takes first out of queue
             curnode = queue.popleft()
             nrofnodes += 1
-            # When you've reached a possible solution checks if better
             if curnode.level == max:
                 if curnode.price < best.price:
                     best = curnode
                     print(best)
                     end = time.time()
                     print(end - start)
-            # branch and bound??
             if curnode.price > best.price:
                 continue
             for node in children(curnode, b, h, distdict, max):
@@ -48,10 +43,8 @@ def children(node, b, h, distdict, max):
     """
     if node.level < max:
         for battery in b:
-            # Prunes unallowed options
             if node.fill[battery][0] + \
                     h[node.level].output <= b[battery].capacity:
-                # Makes copy of parent node and adds the house for every batt
                 cnode = pickle.loads(pickle.dumps(node, -1))
                 cnode.batts[battery].append(cnode.level)
                 cnode.fill[battery][0] += h[cnode.level].output
